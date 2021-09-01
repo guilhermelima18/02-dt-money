0e14ac2be18ad29e808d0202cf506580947fb819
guilhermelima18 <guilhermelima18@hotmail.com>
2021-09-01 00:30:24 -0300

    Criação do Modal de Cadastro de Novas Transações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface ModalProps {
+  onOpenModal: () => void;
+}
+
+export function Header({ onOpenModal }: ModalProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button
+          type="button"
+          onClick={onOpenModal}
+        >
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
